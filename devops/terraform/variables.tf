bfc972b49b0d9edf333a9c5153a3db27c6692fdf
thanoojrules <mirabanayadav@gmail.com>
2025-02-11 00:16:53 -0500

    📌 Restoring all Terraform files

---
+++ devops/terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "location" {
+  description = "Azure Region"
+  default     = "East US"
+}
+
+variable "resource_group_name" {
+  description = "Resource Group Name"
+  default     = "BigBank-RG"
+}
+
+variable "storage_account_name" {
+  description = "Storage Account for Terraform State"
+  default     = "bigbankterraformstate"
+}
