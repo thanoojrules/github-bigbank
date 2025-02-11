b3bc95c1a2041e7c73c03ecbf956e6b68c6e15bc
thanoojrules <71320010+thanoojrules@users.noreply.github.com>
2025-02-11 00:20:52 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,16 @@
 # Terraform variables
+
+variable "location" {
+  description = "Azure Region"
+  default     = "East US"
+}
+
+variable "resource_group_name" {
+  description = "Resource Group Name"
+  default     = "BigBank-RG"
+}
+
+variable "storage_account_name" {
+  description = "Storage Account for Terraform State"
+  default     = "bigbankterraformstate"
+}
