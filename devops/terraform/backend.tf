52d3ac63b8ead45f70608fc6944de33ec167d902
thanoojrules <mirabanayadav@gmail.com>
2025-03-21 14:38:44 -0400

    deployment file

--- devops/terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "BigBank-RG"
-    storage_account_name = "bigbankterraformstate"
-    container_name       = "terraform-state"
-    key                  = "terraform.tfstate"
-  }
-}
